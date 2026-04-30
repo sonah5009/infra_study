9b188a4019ad5e66c26e380fb983a66d5dc62637
sonah5009 <sonah5009@gmail.com>
2026-04-30 02:07:57 +0000

    feat: updating the server to render HTML by default

--- 1. Linux + HTTP + Node.js/server.js
+++ 1. Linux + HTTP + Node.js/server.js
@@ -15,32 +15,24 @@ const server = http.createServer((req, res) => {
       uptime_hours: Number((os.uptime() / 3600).toFixed(2))
     };
 
-    const accept = req.headers.accept || '';
-    if (accept.includes('text/html')) {
-      const htmlPath = path.join(__dirname, 'assets', 'week1.html');
-      fs.readFile(htmlPath, 'utf8', (err, data) => {
-        if (err) {
-          res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
-          res.end('Internal Server Error: Cannot read HTML file.');
-          return;
-        }
+    const htmlPath = path.join(__dirname, 'assets', 'week1.html');
+    fs.readFile(htmlPath, 'utf8', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
+        res.end('Internal Server Error: Cannot read HTML file.');
+        return;
+      }
+      
+      const renderedHtml = data
+        .replace('{{type}}', osInfo.type)
+        .replace('{{hostname}}', osInfo.hostname)
+        .replace('{{cpu_num}}', osInfo.cpu_count)
+        .replace('{{total_mem}}', osInfo.total_memory_mb + ' MB')
+        .replace('{{uptime_hours}}', osInfo.uptime_hours);
         
-        const renderedHtml = data
-          .replace('{{type}}', osInfo.type)
-          .replace('{{hostname}}', osInfo.hostname)
-          .replace('{{cpu_num}}', osInfo.cpu_count)
-          .replace('{{total_mem}}', osInfo.total_memory_mb + ' MB')
-          .replace('{{uptime_hours}}', osInfo.uptime_hours);
-          
-        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-        res.end(renderedHtml);
-      });
-      return;
-    }
-
-    // JSON response for all other clients (default)
-    res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
-    res.end(JSON.stringify(osInfo, null, 2));
+      res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+      res.end(renderedHtml);
+    });
 
   } else if (req.url === '/health' && req.method === 'GET') {
     res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
